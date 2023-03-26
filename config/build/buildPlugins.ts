a987eff805d0945e8561b028c71fec7d07e5c0a5
Gamza222 <turpurture@mail.ru>
2023-03-26 14:10:48 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -10,7 +10,7 @@ export function buildPlugins ({
     paths,
     isDev
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html
         }),
@@ -21,11 +21,20 @@ export function buildPlugins ({
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new ReactRefreshWebpackPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
         })
     ]
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false
+            })
+        )
+        plugins.push(
+            new ReactRefreshWebpackPlugin()
+        )
+    }
+
+    return plugins
 }
